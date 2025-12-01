0797ad4f7a342aefa6516b9fe712a188961a4f4e
manju1907 <lella.manjusri@gmail.com>
2025-12-01 04:07:19 +0000

    fullstack print in react

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,7 @@
+
 import React from "react";
 import { Link } from "react-router-dom";
+
 
 const Card = ({description, alt_description, id, user, urls, likes}) => {
 
@@ -24,4 +26,4 @@ const Card = ({description, alt_description, id, user, urls, likes}) => {
   )
 }
 
-export default Card;
+export default Card;
--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,5 +1,52 @@
-const CardList = () => {
-  return ();
-}
+import Card from './Card'
+import Search from './Search';
+import Button from './Button'
+import React, { useState, useEffect } from "react";
 
-export default CardList;
+const CardList = ({ data }) => {
+
+  const limit = 10;
+  const defaultDataset = data.slice(0, limit);
+  const [offset, setOffset] = useState(0);
+  const [products, setProducts] = useState(defaultDataset)
+
+
+  useEffect(() => {
+  
+  setProducts(data.slice(offset, offset + limit));
+  }, [offset, limit, data]);
+
+
+ const filterTags = (tagQuery) => {
+  const filtered = data.filter(product => {
+
+    // If no tag entered → return all products
+    if (!tagQuery) return product;
+
+    // Check if product.tags contains a tag with matching title
+    return product.tags.find(tag => tag.title === tagQuery);
+  
+  });
+  setOffset(0)
+  setProducts(filtered)
+  }
+
+  return (
+    <div className="cf pa2">
+      <Search handleSearch={filterTags}/>
+      <div className="mt2 mb2">
+
+        {products && products.map((product) => (
+          <Card key={product.id} {...product} />
+        ))}
+      </div>
+
+      <div className="flex items-center justify-center pa4">   
+        <Button text="Previous" handleClick={()=>setOffset(offset - limit)} />
+        <Button text="Next" handleClick={()=>setOffset(offset + limit)} />
+      </div>
+    </div>
+  )
+};
+
+export default CardList;
